7298766a54ddd4bffb0371ea613ac9a6cd660e2e
BENNABI Slimane <slimane.bennabi@edenred.com>
2017-10-26 17:52:31 +0200

    time to go

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import numeral from 'numeral'
 
 const courseValue=numeral(1000).format("$0,0.00")
 
-console.log(`I would get ${courseValue} for this awesome knowledge!`)
+console.log(`I would get ${courseValue} for this awesome knowledge!`)//eslint-disable-line no-console
